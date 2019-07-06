bbc11a7ffa0cc01a435e5eda60af53e5e8c064d3
Anthony Morris <anthony@amorrissound.com>
2019-07-06 12:22:00 -0700

    Convert old GitHub pages to Gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteMetadata: {
+    title: `Anthony Morris | Software Engineer`,
+    description: `Endlessly curious dreamer building products for the future.`,
+    author: `@amorriscode`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `amorriscode`,
+        short_name: `amorriscode`,
+        start_url: `/`,
+        background_color: `#1F1837`,
+        theme_color: `#1F1837`,
+        display: `minimal-ui`,
+        icon: `src/images/male-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
---
+++ src/components/layout.js
@@ -0,0 +1,27 @@
+/**
+ * Layout component that queries for data
+ * with Gatsby's useStaticQuery component
+ *
+ * See: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+
+import "./layout.css"
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <div>
+        <main>{children}</main>
+      </div>
+    </>
+  )
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
