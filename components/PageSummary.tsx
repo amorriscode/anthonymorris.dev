d8eec63f76798a02b52c7226888efa6f2c6de784
Anthony Morris <anthony@amorrissound.com>
2020-09-01 16:43:35 -0700

    Decrease the page summary padding on mobile

--- components/PageSummary.tsx
+++ components/PageSummary.tsx
@@ -2,7 +2,7 @@ function PageSummary({
   children,
 }: React.PropsWithChildren<{}>): React.ReactElement {
   return (
-    <div className="space-y-4 bg-buzz-white bg-opacity-50 rounded-lg rounded-bl-none rounded-br-none border-b-4 border-buzz-green p-8">
+    <div className="space-y-4 bg-buzz-white bg-opacity-50 rounded-lg rounded-bl-none rounded-br-none border-b-4 border-buzz-green p-6 md:p-8">
       {children}
     </div>
   );
