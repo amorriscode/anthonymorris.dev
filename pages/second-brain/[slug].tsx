6d8e5a5f0b23345e2508878a75a599a9575c0626
Anthony Morris <anthony@amorrissound.com>
2020-09-03 15:33:56 -0700

    Ignore the fact that the math function isn't on window

--- pages/second-brain/[slug].tsx
+++ pages/second-brain/[slug].tsx
@@ -12,6 +12,7 @@ import withLayout from "../../components/withLayout";
 
 function BrainEntryPage({ brainEntry }: { brainEntry: BrainEntry }) {
   useEffect(() => {
+    //@ts-ignore
     window.renderMathInElement(document.body);
   }, []);
   return (
