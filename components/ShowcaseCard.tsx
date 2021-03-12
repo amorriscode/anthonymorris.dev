4826a9ca8cb6a3bca6db66351baf1dd8e17174b9
Anthony Morris <amorriscode@gmail.com>
2021-03-12 15:10:38 -0800

    Add hover animation to ShowcaseCard

--- components/ShowcaseCard.tsx
+++ components/ShowcaseCard.tsx
@@ -17,27 +17,35 @@ function ShowcaseCard({
   date: string
 }) {
   return (
-    <Link href={href}>
-      <a className="space-y-2">
-        <div
-          className="bg-am-black w-full h-56 rounded-lg"
-          style={{ backgroundImage: `url(${heroImage})` }}
-        ></div>
+    <>
+      <Link href={href}>
+        <a className="space-y-2">
+          <div
+            className="bg-img bg-am-black w-full h-56 rounded-lg transition-all duration-300"
+            style={{ backgroundImage: `url(${heroImage})` }}
+          ></div>
 
-        <div className="px-4">
-          <div className="flex justify-between items-center">
-            <div className="font-am font-medium">{title}</div>
-            {status && <div className="text-xs">{status}</div>}
-          </div>
-
-          {!status && (
-            <div className="text-xs">
-              written {format(new Date(date), 'MMMM do, y')}
+          <div className="px-4">
+            <div className="flex justify-between items-center">
+              <div className="font-am font-medium">{title}</div>
+              {status && <div className="text-xs">{status}</div>}
             </div>
-          )}
-        </div>
-      </a>
-    </Link>
+
+            {!status && (
+              <div className="text-xs">
+                written {format(new Date(date), 'MMMM do, y')}
+              </div>
+            )}
+          </div>
+        </a>
+      </Link>
+
+      <style jsx>{`
+        a:hover > .bg-img {
+          transform: translateY(-0.5rem);
+        }
+      `}</style>
+    </>
   )
 }
 
