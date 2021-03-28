0349318b240d4079aad587ec7b1a399353faa7ba
Anthony Morris <amorriscode@gmail.com>
2021-03-28 13:06:51 -0700

    Make search overlay bg less transparent

--- components/Search.tsx
+++ components/Search.tsx
@@ -22,7 +22,7 @@ function Search({ handleSearchClose }: { handleSearchClose: () => void }) {
   useOnClickOutside(ref, handleClose)
 
   return (
-    <div className="fixed top-0 bottom-0 left-0 right-0 h-screen w-screen z-50 bg-am-black bg-opacity-10 pt-48">
+    <div className="fixed top-0 bottom-0 left-0 right-0 h-screen w-screen z-50 bg-am-black bg-opacity-20 pt-48">
       <div className="relative w-1/2 mx-auto shadow-2xl" ref={ref}>
         <input
           className="rounded-lg p-5 w-full"
